6b0d7d88064e1ef98003f1fec80d6f889cd5756a
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2025-02-19 23:59:12 +0100

    flag

--- src/zap.c
+++ src/zap.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// TODO process args
+    // support zapping through ssh tunel
+
+// its not that fucking easy, because there is something called file system journaling?
+
+typedef enum {
+    // exclude if whole dir?
+    SSH,
+    DELETE
+} flag;
 
 int main(int argc, char *argv[]) {
     if (argc == 1) {
-        printf("usage: zap [] target_file");
+        printf("usage: zap [OPTION] target_file"); return 1;
+    }
+    char *filename;
+    if (!strcmp(argv[1], "--delete")) {
+        if (argc > 2) {
+            filename = argv[2];
+        } else {
+            printf("usage: zap [OPTION] target_file"); return 1;
+        }
+    } else {
+        filename = argv[1];
     }
 
-    FILE *file = fopen("arg", "r+b");
+    FILE *file = fopen(filename, "r+b");
     if (file == NULL) {
         printf("No such file\n"); return 1;
     }
