713afd49f2aa70dbd200ff0b0f5802f4fda21dcc
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2025-02-23 23:20:51 +0100

    process args

--- src/zap.c
+++ src/zap.c
@@ -2,41 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <limits.h>
+#include <errno.h>
 
-// TODO process args
-    // support zapping through ssh tunel
+// TODO
+// concurrent overwriting
+// support zapping through ssh tunel
 
-// its not that fucking easy, because there is something called file system journaling?
+// its not that fucking easy, because there is something called file system journaling
 
-// typedef enum {
-//     // exclude if whole dir?
-//     SSH,
-//     DELETE
-// } flag;
+typedef struct filenames filenames;
 
-int main(int argc, char *argv[]) {
-    if (argc == 1) {
-        printf("usage: zap [OPTION] target_file"); return 1;
-    }
+struct filenames {
+    char **names;
+    size_t k;
+    size_t capacity;
+};
 
-    char *filename;
-    int del = 0;
-    if (!strcmp(argv[1], "--delete") || !strcmp(argv[1], "-d")) {
-        if (argc > 2) {
-            filename = argv[2]; del = 1;
-        } else {
-            printf("usage: zap [OPTION] target_file"); return 1;
+void add_filename(filenames *arr, char *name) {
+    do {
+        if (arr -> k >= arr -> capacity) {
+            if (arr -> capacity == 0) arr -> capacity = 64;
+            else arr -> capacity *= 2;
+            arr -> names = realloc(arr -> names, arr -> capacity * sizeof(*arr -> names));
         }
-    } else {
-        filename = argv[1];
-    }
+        arr -> names[arr -> k++] = name;
+    } while (0);
+}
 
-    FILE *file = fopen(filename, "rb+");
-    if (file == NULL) {
-        printf("No such file\n"); return 1;
-    }
-
-    srand(time(NULL));
+void overwrite(FILE *file) {
     int c;
     while ((c = fgetc(file)) != EOF) {
         fseek(file, -1, SEEK_CUR);
@@ -44,11 +38,62 @@ int main(int argc, char *argv[]) {
         fputc((char) b, file);
         fflush(file);
     }
-    fclose(file);
+}
 
-    if (del) {
-        remove(filename);
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("usage: zap [-d] [-r reiteration] target_file\n       zap [-d] [-r reiteration] target_directory\n"); return 1;
     }
+    
+    filenames fns = {0};
+    int del = 0, k = 1;
+    for (size_t i = 1; i < argc; i++) {
+        if (!strcmp(argv[i], "-d") || !strcmp(argv[i], "--delete")) del = 1;
+        else if (!strcmp(argv[i], "-r") || !strcmp(argv[i], "--reiteration")) {
+            if ((i + 1) > argc) {
+                printf("usage: zap [-d] [-r reiteration] target_file\n       zap [-d] [-r reiteration] target_directory\n"); return 1;
+            } else {
+                char *p;
+                int rk = strtol(argv[i + 1], &p, 10);
+                if (errno != 0 || *p != '\0') {
+                    printf("zap: Reiteration has to be a number"); return 1;
+                } else if (rk > 10 || rk < 1) {
+                    printf("zap: -r: Reiteration has to be in range <1, 11)"); return 1;
+                } else {
+                    k = rk; i++;
+                }
+            }
+        } else {
+            // TODO add target to list
+            printf("%s", argv[i]);
+            add_filename(&fns, argv[i]);
+        }
+    }
+
+    // TODO for every name
+        // **select how many threads should be used
+        // find if it is a file or dir
+            // if dir, then run overwrite * k for every child
+            // else run overwrite * k
+
+    // FILE *file = fopen(filename, "rb+");
+    // if (file == NULL) {
+    //     printf("zap: %s: No such file or directory", filename); return 1;
+    // }
+
+    // srand(time(NULL));
+    // int c;
+    // while ((c = fgetc(file)) != EOF) {
+    //     fseek(file, -1, SEEK_CUR);
+    //     int b = rand() & 1;
+    //     fputc((char) b, file);
+    //     fflush(file);
+    // }
+    // fclose(file);
+
+    // if (del) {
+    //     remove(filename);
+    // }
 
     return 0;
 }
